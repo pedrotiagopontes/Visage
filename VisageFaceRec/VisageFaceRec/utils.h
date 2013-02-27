e68c9c46d72ab0aa26e26aefc2e70049cc14cef0
Pedro Pontes <pedrotiagopontes@gmail.com>
2013-02-27 19:09:55 +0000

    some more tests

--- VisageFaceRec/VisageFaceRec/utils.h
+++ VisageFaceRec/VisageFaceRec/utils.h
@@ -15,7 +15,7 @@ using namespace cv;
 using namespace std;
 
 static string nomalize_number(string num);
-void read_csv_lfw(const string& filename, vector<string> &names, vector<Mat>& imagesTrain, vector<Mat>& imagesTest, vector<int>& labels, char separator = '\t', string ext = ".pgm");
+void read_csv_lfw(const string& filename, vector<string> &names, vector<Mat>& imagesTrain, vector<Mat>& imagesTest, vector<int>& labels, char separator = '\t', string ext = ".jpg");
 void read_csv(const string& filename, int trainedImgsPerClass, vector<Mat>& imagesTrain, vector<Mat>& imagesTest, vector<int>& labelsTrain, vector<int>& labelsTest, vector<string>& names, char separator = ';');
 void readLfwStats(const string& filename, string outputfilename, vector<int>& labelsPerClass, vector<string>& names);
 void createCSV(string filename, vector<int> imgsPerClass, vector<string> classes, int bottomLimit, int topLimit, string fileExtension = ".jpg");
