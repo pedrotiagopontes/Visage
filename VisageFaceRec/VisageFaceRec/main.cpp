827a36bc4fbc168210b969480a505914b0448d78
Pedro Pontes <pedrotiagopontes@gmail.com>
2013-04-23 15:25:31 +0100

    updated 64bits FaceRecognizer

--- VisageFaceRec/VisageFaceRec/main.cpp
+++ VisageFaceRec/VisageFaceRec/main.cpp
@@ -218,11 +218,11 @@ int main(int argc, const char *argv[]) {
 	*/
 
 	/// CsvCreator.exe
-	return csvCreator(argc, argv);
+	//return csvCreator(argc, argv);
 
 	/// FaceRecognizer.exe
 	//path.txt results.txt [-E(Eingefaces) -F(FisherFaces-default) -L(LBPH)] [nResults]
-	//return faceRecognizer(argc, argv);
+	return faceRecognizer(argc, argv);
 
 	//return 0;
 }
