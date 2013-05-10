f4ad9dfed183a0f7cda7a7e1c0752f8bc050738e
Pedro Pontes <pedrotiagopontes@gmail.com>
2013-05-10 16:56:10 +0100

    cropped masked original x4 results -B

--- VisageFaceRec/x64/testBench_semMasked.bat
+++ VisageFaceRec/x64/testBench_semMasked.bat
@@ -30,19 +30,4 @@ FaceRecognizer.exe ..\data\cropped_20_20_B.txt cropped_20_20_LBPH_B.txt -L 30
 FaceRecognizer.exe ..\data\cropped_20_20_C.txt cropped_20_20_LBPH_C.txt -L 30
 FaceRecognizer.exe ..\data\cropped_20_20_D.txt cropped_20_20_LBPH_D.txt -L 30
 
-REM MASKED
-FaceRecognizer.exe ..\data\masked_20_20_A.txt masked_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\masked_20_20_B.txt masked_20_20_EIGEN_B.txt -E 30
-FaceRecognizer.exe ..\data\masked_20_20_C.txt masked_20_20_EIGEN_C.txt -E 30
-FaceRecognizer.exe ..\data\masked_20_20_D.txt masked_20_20_EIGEN_D.txt -E 30
-
-FaceRecognizer.exe ..\data\masked_20_20_A.txt masked_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\masked_20_20_B.txt masked_20_20_FISHER_B.txt -F 30
-FaceRecognizer.exe ..\data\masked_20_20_C.txt masked_20_20_FISHER_C.txt -F 30
-FaceRecognizer.exe ..\data\masked_20_20_D.txt masked_20_20_FISHER_D.txt -F 30
-
-FaceRecognizer.exe ..\data\masked_20_20_A.txt masked_20_20_LBPH.txt -L 30
-FaceRecognizer.exe ..\data\masked_20_20_B.txt masked_20_20_LBPH_B.txt -L 30
-FaceRecognizer.exe ..\data\masked_20_20_C.txt masked_20_20_LBPH_C.txt -L 30
-FaceRecognizer.exe ..\data\masked_20_20_D.txt masked_20_20_LBPH_D.txt -L 30
 PAUSE
