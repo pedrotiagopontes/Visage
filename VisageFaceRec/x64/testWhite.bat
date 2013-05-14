d488fa39c82f69904807297a38694d615cddc206
Pedro Pontes <pedrotiagopontes@gmail.com>
2013-05-14 13:42:40 +0100

    white balance 4 datasets resutls

--- VisageFaceRec/x64/testWhite.bat
+++ VisageFaceRec/x64/testWhite.bat
@@ -1,13 +1,48 @@
-REM norm_20_20
-FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20.txt norm_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20.txt norm_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20.txt norm_20_20_LBPH.txt -L 30
-REM equalized_20_20
-FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20.txt equalized_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20.txt equalized_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20.txt equalized_20_20_LBPH.txt -L 30
-REM CLAHE_20_20
-FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20.txt CLAHE_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20.txt CLAHE_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20.txt CLAHE_20_20_LBPH.txt -L 30
+REM normalized
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_A.txt normalized_20_20_EIGEN.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_B.txt normalized_20_20_EIGEN_B.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_C.txt normalized_20_20_EIGEN_C.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_D.txt normalized_20_20_EIGEN_D.txt -E 30
+
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_A.txt normalized_20_20_FISHER.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_B.txt normalized_20_20_FISHER_B.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_C.txt normalized_20_20_FISHER_C.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_D.txt normalized_20_20_FISHER_D.txt -F 30
+
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_A.txt normalized_20_20_LBPH.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_B.txt normalized_20_20_LBPH_B.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_C.txt normalized_20_20_LBPH_C.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\normalized_20_20_D.txt normalized_20_20_LBPH_D.txt -L 30
+
+REM equalized
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_A.txt equalized_20_20_EIGEN.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_B.txt equalized_20_20_EIGEN_B.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_C.txt equalized_20_20_EIGEN_C.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_D.txt equalized_20_20_EIGEN_D.txt -E 30
+
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_A.txt equalized_20_20_FISHER.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_B.txt equalized_20_20_FISHER_B.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_C.txt equalized_20_20_FISHER_C.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_D.txt equalized_20_20_FISHER_D.txt -F 30
+
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_A.txt equalized_20_20_LBPH.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_B.txt equalized_20_20_LBPH_B.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_C.txt equalized_20_20_LBPH_C.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\equalized_20_20_D.txt equalized_20_20_LBPH_D.txt -L 30
+
+REM CLAHE
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_A.txt CLAHE_20_20_EIGEN.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_B.txt CLAHE_20_20_EIGEN_B.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_C.txt CLAHE_20_20_EIGEN_C.txt -E 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_D.txt CLAHE_20_20_EIGEN_D.txt -E 30
+
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_A.txt CLAHE_20_20_FISHER.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_B.txt CLAHE_20_20_FISHER_B.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_C.txt CLAHE_20_20_FISHER_C.txt -F 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_D.txt CLAHE_20_20_FISHER_D.txt -F 30
+
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_A.txt CLAHE_20_20_LBPH.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_B.txt CLAHE_20_20_LBPH_B.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_C.txt CLAHE_20_20_LBPH_C.txt -L 30
+FaceRecognizer.exe ..\data\whiteBalance\CLAHE_20_20_D.txt CLAHE_20_20_LBPH_D.txt -L 30
 PAUSE
