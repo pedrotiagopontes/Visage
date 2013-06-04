99526941b3d0872c02951baac15a021c460abae9
Pedro Pontes <pedrotiagopontes@gmail.com>
2013-06-05 00:17:55 +0100

    filters results

--- VisageFaceRec/x64/testFilters.bat
+++ VisageFaceRec/x64/testFilters.bat
@@ -1,9 +1,32 @@
-REM bilateral
-FaceRecognizer.exe ..\data\Filters\bilateral_20_20.txt bilateral_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\Filters\bilateral_20_20.txt bilateral_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\Filters\bilateral_20_20.txt bilateral_20_20_LBPH.txt -L 30
-REM gaussian
-FaceRecognizer.exe ..\data\Filters\gaussian_20_20.txt gaussian_20_20_EIGEN.txt -E 30
-FaceRecognizer.exe ..\data\Filters\gaussian_20_20.txt gaussian_20_20_FISHER.txt -F 30
-FaceRecognizer.exe ..\data\Filters\gaussian_20_20.txt gaussian_20_20_LBPH.txt -L 30
+REM Bilateral
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_A.txt bilateral_20_20_EIGEN.txt -E 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_B.txt bilateral_20_20_EIGEN_B.txt -E 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_C.txt bilateral_20_20_EIGEN_C.txt -E 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_D.txt bilateral_20_20_EIGEN_D.txt -E 30
+
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_A.txt bilateral_20_20_FISHER.txt -F 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_B.txt bilateral_20_20_FISHER_B.txt -F 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_C.txt bilateral_20_20_FISHER_C.txt -F 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_D.txt bilateral_20_20_FISHER_D.txt -F 30
+
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_A.txt bilateral_20_20_LBPH.txt -L 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_B.txt bilateral_20_20_LBPH_B.txt -L 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_C.txt bilateral_20_20_LBPH_C.txt -L 30
+FaceRecognizer.exe ..\data\Filters\bilateral_20_20_D.txt bilateral_20_20_LBPH_D.txt -L 30
+
+REM Gaussian
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_A.txt gaussian_20_20_EIGEN.txt -E 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_B.txt gaussian_20_20_EIGEN_B.txt -E 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_C.txt gaussian_20_20_EIGEN_C.txt -E 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_D.txt gaussian_20_20_EIGEN_D.txt -E 30
+
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_A.txt gaussian_20_20_FISHER.txt -F 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_B.txt gaussian_20_20_FISHER_B.txt -F 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_C.txt gaussian_20_20_FISHER_C.txt -F 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_D.txt gaussian_20_20_FISHER_D.txt -F 30
+
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_A.txt gaussian_20_20_LBPH.txt -L 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_B.txt gaussian_20_20_LBPH_B.txt -L 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_C.txt gaussian_20_20_LBPH_C.txt -L 30
+FaceRecognizer.exe ..\data\Filters\gaussian_20_20_D.txt gaussian_20_20_LBPH_D.txt -L 30
 PAUSE
